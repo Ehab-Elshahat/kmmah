a3ae4ddb34f3e623335f5a4467fee060a32f472f
Ehab Elshahat <61202200+Ehab-Elshahat@users.noreply.github.com>
2023-04-25 18:32:26 +0200

    modif 3

--- js/index.js
+++ js/index.js
@@ -17,29 +17,37 @@ window.addEventListener("scroll", () => {
   }
 
   // Hide Footer Logo When Scroll End
-  if (
-    document.documentElement.scrollTop + window.innerHeight + 400 >=
-    document.documentElement.scrollHeight
-  ) {
-   footerLogo.classList.add("hide");
-  } else {
-       footerLogo.classList.remove("hide");
-  }
+ hideLogo();
 
+ addClassShadowToNavBar()
 
 });
 
+function hideLogo() {
+  if (
+    document.documentElement.scrollTop + window.innerHeight + 10 >=
+    document.documentElement.scrollHeight
+  ) {
+    footerLogo.classList.add("hide");
+  } else {
+    footerLogo.classList.remove("hide");
+  }
+}
+
+hideLogo();
+
+// Go to Top
 scrollTopBtn.addEventListener("click", () => {
   window.scrollTo(0, 0);
 });
 
-
+// Hide logo in Small Screen
 const media = window.matchMedia("(max-width: 991px)");
 
   if (media.matches) {
     window.addEventListener("scroll" , () => {
          if (
-           document.documentElement.scrollTop + window.innerHeight + 900 >=
+           document.documentElement.scrollTop + window.innerHeight + 950 >=
            document.documentElement.scrollHeight
          ) {
            footerLogo.classList.add("hide");
@@ -49,6 +57,17 @@ const media = window.matchMedia("(max-width: 991px)");
     })
   } 
 
+  // Add Class To NavBar When Scroll
+  let navBar = document.querySelector(".navbar");
+function addClassShadowToNavBar() {
+  let scrollValue = window.scrollY;
+   if (scrollValue > 0) {
+     navBar.classList.add("shadow-bottom")
+    } else {
+     navBar.classList.remove("shadow-bottom");
+     
+   }
+}
 
 
 
