f7b551010b4d54a9fd7d27d2611927350c00c37b
Ehab Elshahat <61202200+Ehab-Elshahat@users.noreply.github.com>
2023-05-02 09:56:04 +0300

    Add Alerts Valid Email

--- js/index.js
+++ js/index.js
@@ -13,88 +13,103 @@ window.addEventListener("scroll", () => {
   } else {
     scrollTopBtn.classList.remove("active");
   }
- addClassShadowToNavBar()
+  addClassShadowToNavBar();
 });
-
-;
 
 // Go to Top
 scrollTopBtn.addEventListener("click", () => {
   window.scrollTo(0, 0);
 });
 
- 
+// Add Class To NavBar When Scroll
+let navTopImage = document.querySelector(".top-nav-img");
 
-  // Add Class To NavBar When Scroll
-  let navTopImage = document.querySelector(".top-nav-img");
-  console.log(navTopImage.style.height)
-  let navBar = document.querySelector(".navbar");
+let navBar = document.querySelector(".navbar");
 function addClassShadowToNavBar() {
   let scrollValue = window.scrollY;
-   if (scrollValue > 0) {
-     navBar.classList.add("shadow-bottom", "move-top");
-     navTopImage.style.height = 0
-    } else {
-     navBar.classList.remove("shadow-bottom", "move-top");
-     navTopImage.style.height= "111px"
-     
-   }
+  if (scrollValue > 0) {
+    navBar.classList.add("shadow-bottom", "move-top");
+    navTopImage.style.height = 0;
+  } else {
+    navBar.classList.remove("shadow-bottom", "move-top");
+    navTopImage.style.height = "111px";
+  }
 }
 addClassShadowToNavBar();
 
 // Hide Collapsed menu After Clicking OutSide It
 let navbarCollapse = document.querySelector(".navbar-collapse");
-document.addEventListener("keydown" ,(e)=> {
- 
- if(e.key === "Escape"){
-  navbarCollapse.classList.remove("show")
- }
-})
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    navbarCollapse.classList.remove("show");
+  }
+});
 
 // Go To Page On Click a Link
 function goToLink(e) {
   window.open(e, "_self");
 }
 
-// Stop Scrolling When navbar open in small screen
-let htmlElement = document.documentElement;
+// let navToggler = document.querySelector(".navbar-toggler");
 
+// navToggler.addEventListener("click", ()=> {
 
-let navToggler = document.querySelector(".navbar-toggler");
-
-navToggler.addEventListener("click", ()=> {
-  htmlElement.classList.toggle("remove-scrollY");
-  handelPosition();
-})
-
+//   // handelPosition();
+// })
 
 // Close Navbar in Small Screen
 let closeNav = document.querySelector(".close-nav");
 
 closeNav.addEventListener("click", () => {
   navbarCollapse.classList.remove("show");
-  htmlElement.classList.remove("remove-scrollY");
-   
-})
-
+});
 
 // Handel nav menu position in small screen
 function handelPosition() {
-  if(window.scrollY > 100) {
-   navbarCollapse.classList.remove("move-top");
+  if (window.scrollY > 100) {
+    navbarCollapse.classList.remove("move-top");
   } else {
-    
     navbarCollapse.classList.add("move-top");
   }
 }
 
 // Stop Click Event On DropDown
-let navItemDropdown = document.querySelector(".nav-item.dropdown "); 
+let navItemDropdown = document.querySelector(".nav-item.dropdown ");
 let dropDownMenu = document.querySelector(".dropdown-menu");
-navItemDropdown.addEventListener("click", (e)=> {
-e.target.parentElement.classList.remove("show")
-dropDownMenu.classList.remove("show")
-})
+navItemDropdown.addEventListener("click", (e) => {
+  e.target.parentElement.classList.remove("show");
+  dropDownMenu.classList.remove("show");
+});
 
+// Validation Email
+const submitForm = document.querySelector(".subscrip-form");
+const subscripInput = document.querySelector(".subscrip-input");
+const successAlert = document.querySelector(".alert.success");
+const dangerAlert = document.querySelector(".alert.danger");
+const warningAlert = document.querySelector(".alert.warning")
+let EmailIsExist = false
+submitForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let mail_format = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  if (subscripInput.value.match(mail_format)) {
+    successAlert.style.display = "flex";
+    setTimeout(() => {successAlert.style.display = "none"}, 3000);
+    return true;
+  } else if(EmailIsExist == true) {
+    warningAlert.style.display="flex";
+     subscripInput.focus();
+     setTimeout(() => {
+       warningAlert.style.display = "none";
+     }, 3000);
+  } else {
+    dangerAlert.style.display = "flex";
+    subscripInput.focus();
+    setTimeout(() => {
+      dangerAlert.style.display = "none";
+    }, 3000);
+    return false;
+  }
+});
 
 
